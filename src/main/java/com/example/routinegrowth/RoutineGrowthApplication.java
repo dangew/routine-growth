72bb6275090d1391da3ffcb1916f7b6097e83136
dangew <dangew99@gmail.com>
2025-03-26 10:47:10 +0900

    style: format code for consistency

--- src/main/java/com/example/routinegrowth/RoutineGrowthApplication.java
+++ src/main/java/com/example/routinegrowth/RoutineGrowthApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoutineGrowthApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RoutineGrowthApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(RoutineGrowthApplication.class, args);
+  }
 }
