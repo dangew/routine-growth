884a524579a01632778912202b0ae36237ae3842
dangew <dangew99@gmail.com>
2025-03-27 16:16:01 +0900

    feat: add entity classes for Routine, RoutineCategory, and User

---
+++ src/main/java/com/example/routinegrowth/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.routinegrowth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity @Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter @Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+}
