8b864cedf673c7061584f90ce854d94ab0e980aa
dangew <dangew99@gmail.com>
2025-03-28 15:17:07 +0900

    "chore: 프로젝트 구조를 backend/frontend 모노레포로 변경 및 React 환경 구성"

--- src/main/java/com/example/routinegrowth/repository/RoutineCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.routinegrowth.repository;
-
-import com.example.routinegrowth.entity.RoutineCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoutineCategoryRepository extends JpaRepository<RoutineCategory, Long> {}
