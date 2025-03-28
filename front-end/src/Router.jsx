326b82041514d4fb87558db6aaa6e5d4d09ef955
dangew <dangew99@gmail.com>
2025-03-28 17:59:41 +0900

    feat: implement ProtectedRoute for user authentication in routing

--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -3,15 +3,37 @@ import LoginPage from "./pages/LoginPage";
 import RoutineListPage from "./pages/RoutineListPage.jsx";
 import RoutineCreatePage from "./pages/RoutineCreatePage";
 import MyPage from "./pages/MyPage";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
-function AppRouter() {
+function AppRouter({ user, setUser }) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<RoutineListPage />} />
-        <Route path="/create" element={<RoutineCreatePage />} />
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<LoginPage setUser={setUser} />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute user={user} setUSer={setUser}>
+              <RoutineListPage setUser={setUser} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create"
+          element={
+            <ProtectedRoute user={user} setUSer={setUser}>
+              <RoutineCreatePage setUser={setUser} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/mypage"
+          element={
+            <ProtectedRoute user={user} setUSer={setUser}>
+              <MyPage setUser={setUser} />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
