6ff4da12bcd1d466f5f4f88c318ebef8d0f6c1a9
dangew <dangew99@gmail.com>
2025-03-28 17:50:36 +0900

    feat: set logged-in user data in localstorage

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,17 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import AppRouter from "./Router";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  return <AppRouter />;
+  const [user, setUser] = useState();
+
+  // get logged-in user data from localstorage
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) setUser(user);
+  }, []);
+
+  return <AppRouter user={user} setUser={setUser} />;
 }
 
 export default App;
