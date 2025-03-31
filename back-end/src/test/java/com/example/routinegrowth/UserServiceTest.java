9412ee235692ddbb6550446a279944d3d410d815
dangew <dangew99@gmail.com>
2025-03-31 17:06:14 +0900

    feat: add failure test for user registration in UserServiceTest

--- back-end/src/test/java/com/example/routinegrowth/UserServiceTest.java
+++ back-end/src/test/java/com/example/routinegrowth/UserServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.routinegrowth;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.example.routinegrowth.DTO.UserRequest;
 import com.example.routinegrowth.DTO.UserResponse;
 import com.example.routinegrowth.service.UserService;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @SpringBootTest
 @DisplayName("UserService Test")
 @TestMethodOrder(MethodOrderer.Random.class)
@@ -23,7 +27,7 @@ public class UserServiceTest {
 
   @Test
   @DisplayName("Register User Test : Success")
-  public void registerUserTest() {
+  public void registerUserTest_success() {
     // make user request object
     UserRequest userRequest = UserRequest.builder().email("register@test").build();
 
@@ -33,5 +37,19 @@ public class UserServiceTest {
     // expectation
     assertThat(user_created).isNotNull();
     assertThat(user_created.getEmail()).isEqualTo(userRequest.getEmail());
+  }
+
+  @Test
+  @DisplayName("Register User Test : Fail")
+  public void registerUserTest_fail() {
+    // make empty user request object
+    UserRequest userRequest = new UserRequest();
+
+    assertThrows(
+        ConstraintViolationException.class,
+        () -> {
+          // register user with empty request
+          userService.createUser(userRequest);
+        });
   }
 }
