1981eac7e79520e2d6c24cfc9bb7bd150774bce9
dangew <dangew99@gmail.com>
2025-04-01 09:57:21 +0900

    feat: add findByEmail method to UserRepository for user retrieval by email

--- back-end/src/main/java/com/example/routinegrowth/repository/UserRepository.java
+++ back-end/src/main/java/com/example/routinegrowth/repository/UserRepository.java
@@ -1,8 +1,17 @@
 package com.example.routinegrowth.repository;
 
 import com.example.routinegrowth.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+  /**
+   * Find user by email
+   *
+   * @param email email of the user
+   * @return Optional<User>
+   */
+  Optional<User> findByEmail(String email);
+}
